fa2bb9015b10e26e71368c284df87cbcc3fe99de
Lucas Felipe <62124129+lucasfelipedonascimento@users.noreply.github.com>
2025-01-17 19:07:33 +0000

    alter description

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ddaf6b6d6072998c"; }
+/******/ 		__webpack_require__.h = function() { return "2e4baa55bfdb846e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
